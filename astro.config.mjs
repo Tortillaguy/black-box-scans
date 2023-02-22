ed38865541e068955ce45c258ca682cc1a59247d
Adrian <adrianc@meetparcel.com>
2023-02-22 15:27:51 -0800

    fix site var

--- astro.config.mjs
+++ astro.config.mjs
@@ -6,6 +6,6 @@ import tailwind from "@astrojs/tailwind";
 // https://astro.build/config
 export default defineConfig({
   integrations: [tailwind()],
-  site: "blackbox3dscans.com",
+  site: "https://www.blackbox3dscans.com",
   base: "/",
 });
